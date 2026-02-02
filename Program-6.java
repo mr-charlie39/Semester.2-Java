0de93d49566522445b638f025969d7effae9c75a
Muhammad Ali Abid <mali1122330309@gmail.com>
2026-02-02 19:21:03 +0500

    New File

--- Program-6.java
+++ Program-6.java
@@ -1,22 +1,25 @@
 import java.util.Scanner;
 
-class Calculation{
+class Calculation {
     int x;
     int y;
 
-    public Calculation(int x , int y){
+    public Calculation(int x, int y) {
         this.x = x;
         this.y = y;
         System.out.println("(" + x + "," + y + ")");
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         System.out.print("Enter first number: ");
         int a = sc.nextInt();
+
         System.out.print("Enter second number: ");
         int b = sc.nextInt();
 
-        Calculation c1 = new Calculation(a , b);
+        Calculation c1 = new Calculation(a, b);
+        Calculation c2 = new Calculation(4, 3);
     }
 }
