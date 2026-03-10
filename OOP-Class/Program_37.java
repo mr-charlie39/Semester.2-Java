a5c7d71bc0e58abc76852528cd79431d0eb13062
Muhammad Ali <mali1122330309@gmail.com>
2026-03-10 13:53:00 +0500

    New File

---
+++ OOP-Class/Program_37.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+abstract class Shape{
+    double length;
+    double breadth;
+    abstract int area();
+    abstract int perimeter();
+    abstract void display();
+    abstract void input();
+}
+
+class input extends Shape{
+    Scanner sc = new Scanner(System.in);
+    void input(){
+        System.out.print("Enter the length : ");
+        length = sc.nextDouble();
+        System.out.print("Enter the breadth : ");
+        breadth = sc.nextDouble();
+    }
+    int area(){
+        return (int)(length * breadth);
+    }
+    int perimeter(){
+        return (int)(2 * (length + breadth));
+    }
+    void display(){
+        System.out.println("Area of the rectangle: " + area());
+        System.out.println("Perimeter of the rectangle: " + perimeter());
+    }
+}
+
+public class Program_37 {
+    public static void main(String[] args) {
+        input rect = new input();
+        rect.input();
+        rect.display();
+    }
+}
